a7698017c2c4d68b1f7ea7c3ff67e0cb674a2423
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-05-27 13:41:46 +0530

    learning React

---
+++ react1/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ react1/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h4>This is the Header</h4>
+    </div>
+  );
+}
+
+export default Header;
