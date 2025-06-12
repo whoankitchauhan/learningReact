dbd71006edbffa977d71fda4e79c71afddb0e4a0
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-12 23:37:10 +0530

    Checkbox

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,54 +1,11 @@
 import { useState } from "react";
+import Skills from "./Skills";
 
 function App() {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
-  const [mail, setMail] = useState();
-
   return (
     <div>
-      <h1>Controlled Compoent</h1>
-      <form action="" method="get">
-        <input
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <br />
-
-        <input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <br />
-
-        <input
-          type="text"
-          placeholder="Enter Email"
-          value={mail}
-          onChange={(e) => setMail(e.target.value)}
-        />
-        <br />
-        <br />
-        <button>Submit</button>
-        <button
-          onClick={() => {
-            setName("");
-            setMail("");
-            setPassword("");
-          }}
-        >
-          Clear
-        </button>
-        <h2>{name}</h2>
-        <h2>{password}</h2>
-        <h2>{mail}</h2>
-      </form>
+      <h1>Handle Checkbox</h1>
+      <Skills />
     </div>
   );
 }
---
+++ react1/src/Skills.jsx
@@ -0,0 +1,22 @@
+function Skills() {
+  return (
+    <div>
+      <h3>Select Your Skills</h3>
+      <input type="checkbox" id="PHP" value="PHP" />
+      <label htmlFor="PHP">PHP</label>
+      <br />
+
+      <input type="checkbox" id="JAVA" value="JAVA" />
+      <label htmlFor="JAVA">JAVA</label>
+      <br />
+
+      <input type="checkbox" id="UIUX" value="UIUX" />
+      <label htmlFor="UIUX">UIUX</label>
+      <br />
+      <input type="checkbox" id="DSA" value="DSA" />
+      <label htmlFor="DSA">DSA</label>
+      <br />
+    </div>
+  );
+}
+export default Skills;
