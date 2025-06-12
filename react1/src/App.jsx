e7b9391c095eb523ff6cd1f5afeb2441ca02a0c2
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-12 21:58:25 +0530

    .

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,21 +1,48 @@
-import User from "./User";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState({
+    name: "ankit",
+    age: 23,
+    email: "ankit@gmail.com",
+  });
   return (
     <div>
-      <User>
-        <h1 style={{ color: "orange" }}> Hey 1</h1>
-      </User>
-      <User>
-        <h1> Hey 2</h1>
-      </User>
-      <User>
-        <h1 style={{ color: "green" }}> Hey 3</h1>
-      </User>
-
-      <User color="purple">
-        <h1> Hey 4</h1>
-      </User>
+      <h1>Input Field</h1>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value.name)}
+        value={value.name}
+        placeholder="Enter Your Name"
+      />
+      <br />
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value.age)}
+        value={value.age}
+        placeholder="Enter Your Age"
+      />
+      <br />
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value.email)}
+        value={value.email}
+        placeholder="Enter Your Mail"
+      />
+      <button
+        onClick={() =>
+          setValue({
+            name: "",
+            age: 0,
+            email: "",
+          })
+        }
+      >
+        Clear
+      </button>
+      <h2>{value.name}</h2>
+      <h2>{value.age}</h2>
+      <h2>{value.email}</h2>
     </div>
   );
 }
