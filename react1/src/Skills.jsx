c8b246cfda5b4093ca4adcfdff4bca68f538532d
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-13 15:08:23 +0530

    checkbox

--- react1/src/Skills.jsx
+++ react1/src/Skills.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
 function Skills() {
+  const [skills, setSkills] = useState([]);
+  const handleSkills = (e) => {
+    console.log(e.target.value, e.target.checked);
+    if (e.target.checked) {
+      setSkills([...skills, e.target.value]);
+    } else {
+      setSkills(skills.filter((item) => item !== e.target.value));
+    }
+  };
   return (
     <div>
       <h3>Select Your Skills</h3>
-      <input type="checkbox" id="PHP" value="PHP" />
+      <input onChange={handleSkills} type="checkbox" id="PHP" value="PHP" />
       <label htmlFor="PHP">PHP</label>
       <br />
 
-      <input type="checkbox" id="JAVA" value="JAVA" />
+      <input onChange={handleSkills} type="checkbox" id="JAVA" value="JAVA" />
       <label htmlFor="JAVA">JAVA</label>
       <br />
 
-      <input type="checkbox" id="UIUX" value="UIUX" />
+      <input onChange={handleSkills} type="checkbox" id="UIUX" value="UIUX" />
       <label htmlFor="UIUX">UIUX</label>
       <br />
-      <input type="checkbox" id="DSA" value="DSA" />
+      <input onChange={handleSkills} type="checkbox" id="DSA" value="DSA" />
       <label htmlFor="DSA">DSA</label>
       <br />
+      <h1>{skills.toString()}</h1>
     </div>
   );
 }
