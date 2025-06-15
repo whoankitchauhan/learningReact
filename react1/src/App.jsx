c42e188e1ca0991e54654ca7cadfe18e1b84218b
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-15 18:40:46 +0530

    Module CSS

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,101 +1,10 @@
-import "./css/style.css";
+import UserProfile from "./userProfile";
 
 function App() {
   return (
     <>
-      <h1 className="heading">External Style</h1>
-      <div className="main-div">
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="img" src="https://picsum.photos/200" alt="" />
-          </div>
-          <div>
-            <h2>Ankit Chauhan</h2>
-            <p>Software Developer</p>
-          </div>
-        </div>
-      </div>
+      <h1>Style With Module CSS</h1>
+      <UserProfile />
     </>
   );
 }
---
+++ react1/src/UserProfile.jsx
@@ -0,0 +1,100 @@
+import style from "./css/userProfile.module.css";
+
+function UserProfile() {
+  return (
+    <div className={style.mainDiv}>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div>
+          <img className="img" src="https://picsum.photos/200" alt="" />
+        </div>
+        <div>
+          <h2>Ankit Chauhan</h2>
+          <p>Software Developer</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserProfile;
