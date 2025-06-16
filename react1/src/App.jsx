818d504696c8349f23832db597c09a909e960a55
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-16 23:43:35 +0530

    UseFormStatus

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,20 +1,31 @@
-import React, { useRef } from "react";
-import ChildInput from "./ChildInput";
-
+import { useFormStatus } from "react-dom";
 function App() {
-  const inputRef = useRef();
-
-  const focusInput = () => {
-    inputRef.current.focus();
+  const handleSubmit = async () => {
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("Submit");
   };
 
+  function Customerform() {
+    const { pending } = useFormStatus();
+    console.log(pending);
+    return (
+      <div>
+        <form action={handleSubmit}>
+          <input type="text" placeholder="Enter Username" />
+          <br />
+          <input type="password" placeholder="Enter Password" />
+          <br />
+          <button disabled={pending}>
+            {pending ? "Submitting..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>forwardRef Example</h1>
-      <ChildInput ref={inputRef} />
-      <br />
-      <br />
-      <button onClick={focusInput}>Focus on Child Input</button>
+      <h1>UseFormStatus</h1>
+      <Customerform />
     </div>
   );
 }
