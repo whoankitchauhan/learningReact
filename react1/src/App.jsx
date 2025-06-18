c9a54c8f4fb519f0d6372da123735158fb8973b3
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-19 00:14:03 +0530

    Updating Objects in States

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
 
-import AddUser from "./addUser";
-import DisplayUser from "./displayUser";
-
 function App() {
-  const [user, setUser] = useState();
+  const [data, setData] = useState({
+    name: "Ankit Chauhan",
+    address: {
+      city: "Delhi",
+      country: "India",
+    },
+  });
+  const handleName = (inputName) => {
+    setData((prevData) => ({
+      ...prevData,
+      name: inputName,
+    }));
+  };
 
+  const handleCity = (inputCity) => {
+    setData((prevData) => ({
+      ...prevData,
+      address: {
+        ...prevData.address,
+        city: inputCity,
+      },
+    }));
+  };
   return (
     <div>
-      <AddUser setUser={setUser}/>
+      <h1>Updating Objects in States</h1>
+      <input
+        type="text"
+        onChange={(event) => handleName(event.target.value)}
+        placeholder="Enter User Name"
+      />
       <hr />
-      <DisplayUser user={user}/>
+      <input
+        type="text"
+        onChange={(event) => handleCity(event.target.value)}
+        placeholder="Enter City"
+      />
+      <h3>Name: {data.name}</h3>
+      <h3>City: {data.address.city}</h3>
+      <h3>Country: {data.address.country}</h3>
     </div>
   );
 }
