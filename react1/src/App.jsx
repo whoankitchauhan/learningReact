b099d5b8552c39ebfa460f6565403e5d7f012255
Ankit Chauhan <whoankitchauhan@gmail.com>
2025-06-21 22:47:58 +0530

    Simple Header with React Router

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,20 +1,19 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
-
+    <>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/about" element={<h1>About Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
---
+++ react1/src/NavBar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "./css/Header.css";
+function NavBar() {
+  return (
+    <div className="header">
+      <div>
+        <Link className="link">
+          <h2>LOGO </h2>
+        </Link>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/About">
+              About
+            </Link>
+          </li>{" "}
+          <li>
+            <Link className="link" to="/Contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
